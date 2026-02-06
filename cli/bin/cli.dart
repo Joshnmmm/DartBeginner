764517815101a3099a4bd14d8ad9660b69654b74
Josh Levin <joshhlevinlopez@gmail.com>
2026-02-06 14:51:11 +0800

    One line function like in JS

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -2,4 +2,8 @@ void main() {
   dynamic name = "Josh"; //dynamic allows variables to change type, nice
   name = 30;
   print(name);
+  String greet = greeting();
+  print(greet);
 }
+
+String greeting() => "Hello"; //one line function just like in JS
